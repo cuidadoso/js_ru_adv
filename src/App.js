4efb90d827a40e8d37edecb74b9533330e740bd9
Alexander <alexander_s@front.ru>
2018-05-30 22:26:28 +0300

    Init config

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import {Provider} from 'react-redux'
+import {ConnectedRouter} from 'react-router-redux'
+
+import Root from './components/Root'
+import store from './redux'
+import history from "./history";
 
 class App extends Component {
   render() {
     return (
-      <div >
-          Hello world!
-      </div>
+      <Provider store={store}>
+          <ConnectedRouter history ={history}>
+              <Root/>
+          </ConnectedRouter>
+      </Provider>
     );
   }
 }
---
+++ src/components/Root.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Root extends Component {
+  static defaultProps = {
+
+  };
+
+  static propTypes = {
+
+  };
+
+  state = {
+
+  };
+
+  render() {
+    return (
+      <div>
+          Hello
+      </div>
+    );
+  }
+}
+
+export default Root;
