553862f9bdacab5b4de92fe1e8f7347aaffe634c
Alexander Pyreev <a.s.pyreev@gmail.com>
2018-06-15 17:51:47 +0300

    Add react-motion

--- src/components/events/SelectedEvents.js
+++ src/components/events/SelectedEvents.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { TransitionMotion, spring } from 'react-motion';
 
 import { selectedEventsSelector } from '../../ducks/events';
 import SelectedEventCard from './SelectedEventCard';
@@ -15,13 +16,41 @@ class SelectedEvents extends Component {
 
   state = {};
 
+  willLeave = () => ({
+    opacity: spring(0, { stiffness: 100 })
+  });
+
+  willEnter = () => ({
+    opacity: 0
+  });
+
+  getStyles() {
+    return this.props.events.map((event) => ({
+      style: {
+        opacity: spring(1, { stiffness: 50 })
+      },
+      key: event.uid,
+      data: event
+    }));
+  }
+
   render() {
     return (
-      <div>
-        {this.props.events.map((event) => (
-          <SelectedEventCard event={event} key={event.uid} />
-        ))}
-      </div>
+      <TransitionMotion
+        styles={this.getStyles()}
+        willLeave={this.willLeave}
+        willEnter={this.willEnter}
+      >
+        {(interpolated) => (
+          <div>
+            {interpolated.map((config) => (
+              <div style={config.style} key={config.key}>
+                <SelectedEventCard event={config.data} />
+              </div>
+            ))}
+          </div>
+        )}
+      </TransitionMotion>
     );
   }
 }
--- src/components/events/Trash.js
+++ src/components/events/Trash.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { DropTarget } from 'react-dnd';
 import { connect } from 'react-redux';
+import { Motion, spring, presets } from 'react-motion';
+
 import { deleteEvent, stateSelector } from '../../ducks/events';
 import Loader from '../common/Loader';
 
@@ -25,11 +27,25 @@ class Trash extends Component {
       top: 0,
       right: 0
     };
-    return connectDropTarget(
-      <div style={{ ...style }}>
-        Trash
-        {loading && <Loader />}
-      </div>
+    return (
+      <Motion
+        defaultStyle={{ opacity: 0 }}
+        style={{
+          opacity: spring(1, {
+            damping: presets.noWobble.damping * 4,
+            stiffness: presets.noWobble.stiffness / 2
+          })
+        }}
+      >
+        {(interpolatedStyle) =>
+          connectDropTarget(
+            <div style={{ ...style, ...interpolatedStyle }}>
+              Trash
+              {loading && <Loader />}
+            </div>
+          )
+        }
+      </Motion>
     );
   }
 }
